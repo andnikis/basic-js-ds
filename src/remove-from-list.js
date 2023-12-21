c02af43762cf3700d3088fbf25f699424cdafc00
andnikis <andreymon@gmail.com>
2023-12-22 00:55:07 +0600

    feat: do remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  // the first node, l must be defined
+  while (l !== null && l.value === k) l = l.next;
+
+  // save the head in l
+  let list = l;
+  while (list !== null) {
+    if (list.next !== null && list.next.value === k) {
+      list.next = list.next.next;
+    }
+    else {
+      list = list.next;
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
